779ecdb847fef5959d637fd168a77e4a6714a51e
vũ nguyễn <imvms205@gmail.com>
2020-03-07 16:49:42 +0700

    test

--- request_logging/middleware.py
+++ request_logging/middleware.py
@@ -171,7 +171,7 @@ class LoggingMiddleware(object):
     def _log_request(self, request):
         # method_path = "{} {}".format(request.method, request.get_full_path())
         #
-        # logging_context = self._get_logging_context(request, None)
+        logging_context = self._get_logging_context(request, None)
         # self.logger.log(logging.INFO, method_path, logging_context)
         # Ted modify: disable response header
         # self._log_request_headers(request, logging_context)
